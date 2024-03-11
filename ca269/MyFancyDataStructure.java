338d3815891af8dd4b289f5e822be4301753a0ee
whelas64 <shane.whelan64@mail.dcu.ie>
2024-03-11 01:41:49 +0000

    ca269 assignment 4

--- ca269/MyFancyDataStructure.java
+++ ca269/MyFancyDataStructure.java
@@ -2,6 +2,7 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.Collections;
 import java.lang.Comparable;
+import java.util.Comparator;
 
 
 class Value implements Comparable<Value>{
@@ -49,6 +50,8 @@ class Value implements Comparable<Value>{
 
     }
 
+
+    // the following method can also be done using the integer compare method but judging by the wording of the question, I think this is what is required
     public int compareTo(Value o) {
             if (SORT_LOWER) {
                 if (this.value < o.value) {
@@ -70,17 +73,33 @@ class Value implements Comparable<Value>{
         }
 }
 
+
+ // the following classes use the integer compare method but as shown previously I do know how to complete a comparison without it
+class PreferLowerValues implements Comparator<Value> {
+    public int compare(Value v1, Value v2) {
+        return Integer.compare(v1.getValue(), v2.getValue());
+    }
+}
+
+class PreferHigherValues implements Comparator<Value> {
+    public int compare(Value v1, Value v2) {
+        return Integer.compare(v2.getValue(), v1.getValue());
+    }
+}
+
+
+
+
 class MyFancyDataStructure {
     public static void main(String[] args) {
-        List<Value> list = Arrays.asList(new Value(2), new Value(3), new Value(1));
-
-        Value.setSortLower();
-        Collections.sort(list);
+        List<Value> list = Arrays.asList(
+            new Value(2), new Value(3), new Value(1));
+    
+        list.sort(new PreferLowerValues());
         System.out.println(list);
         // OUTPUT: [1, 2, 3]
-
-        Value.setSortHigher();
-        Collections.sort(list);
+    
+        list.sort(new PreferHigherValues());
         System.out.println(list);
         // OUTPUT: [3, 2, 1]
     }
